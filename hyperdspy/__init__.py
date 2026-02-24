4053542b8c02a3b157533d3ab8dbae7c9cd601ed
Tripudium <martin@tripudium.tech>
2026-02-24 09:31:35 +0000

    Minor fixes

--- hyperdspy/__init__.py
+++ hyperdspy/__init__.py
@@ -1,12 +1,12 @@
 from hyperdspy.config import Config as Config
 from hyperdspy.config import load_config
+from hyperdspy.engine import Engine
+from hyperdspy.strategies.simple_mm import SimpleMarketMaker
 
 
 def main():
     """CLI entry point: `uv run dspy`"""
-    from hyperdspy.engine import Engine
-    from hyperdspy.strategies.simple_mm import SimpleMarketMaker
-
+    
     config = load_config()
     strategy = SimpleMarketMaker()
     engine = Engine(config, strategy)
